d4f77951ae4b019b2d008e04252c7f744289eb03
German Contreras <inginf.german.fernandez@gmail.com>
2023-03-26 12:02:55 -0300

    add folder for one hooks

---
+++ useTodo/todoReducer.js
@@ -0,0 +1,31 @@
+
+export const todoReducer = (initialState = [], action) => {
+
+    switch (action.type) {
+        case '[Todo] Add Todo':
+            return [...initialState, action.payload];
+
+        case '[Todo] Delete Todo':
+            return initialState.filter(todo => todo.id !== action.payload);
+
+        case '[Todo] Toggle Todo':
+            return initialState.map(todo => {
+                if (todo.id === action.payload) {
+                    return {
+                        ...todo,
+                        done: !todo.done
+                    }
+                }
+                return todo
+            })
+
+        default:
+            return initialState;
+    }
+
+
+    return initialState;
+}
+
+
+
